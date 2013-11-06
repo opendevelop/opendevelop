dea0a2a55a02e32c2e0d071924d0515d729afd65
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-06 02:19:57 +0200

    Updated requirements.txt Updated django settings

--- opendevelop/opendevelop/settings.py
+++ opendevelop/opendevelop/settings.py
@@ -9,10 +9,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+APP_DIR = '/etc/opendevelop'
+DB_NAME = '%s/db' % APP_DIR
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DB_NAME,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
