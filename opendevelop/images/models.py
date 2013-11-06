6e6c3ee3915a72b1e7f4649b554c63b4105d943d
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-06 16:48:28 +0200

    Started Images model

--- opendevelop/images/models.py
+++ opendevelop/images/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    """
+    This class defines the Image mode. Each sandbox should be created with an
+    Image as a base. Each Image should be have a valid counterpart to every
+    Docker server connected to OpenDevelop
+    """
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
+    description = models.TextField()
+    url = models.URLField(max_length=64)
+    docker_image_name = models.CharField(max_length=32)
