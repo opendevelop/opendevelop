5c56bd568630242eb41cb9b68cd4f6a2faaa9a7e
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-06 02:06:09 +0200

    Installer adds root user and sudo (if it exists) to opendevelop group

--- install/installer.py
+++ install/installer.py
@@ -5,6 +5,7 @@ of OpenDevelop.
 """
 
 from getpass import getuser
+from os import environ
 from subprocess import call
 from subprocess import PIPE
 from sys import exit
@@ -33,3 +34,17 @@ if (call(['id', USER_NAME], stdout=PIPE, stderr=PIPE)):
     print '  Okay.'
 else:
     print '  It seems like the user already exists. Skipping...'
+
+# Add current user and sudo user (if exists) to opendevelop group
+OPENDEVELOP_USERS = [USER]
+if ('SUDO_USER' in environ):
+    OPENDEVELOP_USERS.append(environ['SUDO_USER'])
+for od_user in OPENDEVELOP_USERS:
+    print 'Adding user %s to group %s' % (od_user, USER_NAME)
+    args = ['usermod', '-aG', USER_NAME, od_user]
+    result = call(args, stdout=PIPE, stderr=PIPE)
+    if (result):
+        die('  Cannot add user %s to opendevelop group. Exiting.' % od_user)
+    print '  Okay.'
+
+# Create application directory and give proper rights
