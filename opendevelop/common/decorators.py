5b2f7e9e12cc58ff19a2021f730c3c97ca4ff868
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-11 23:12:31 +0200

    Ruined it

--- opendevelop/common/decorators.py
+++ opendevelop/common/decorators.py
@@ -1,5 +1,6 @@
 from common.views import HttpResponseUnauthorized
 from functools import wraps
+import base64
 
 def oauth(view):
     @wraps(view)
@@ -8,5 +9,6 @@ def oauth(view):
         if (not AUTH_HEADER_KEY in request.META):
             msg = 'This resource requires OAuth2 authorization.'
             return HttpResponseUnauthorized(msg)
+        auth_data = base64.standard_b64decode(request.META[AUTH_HEADER_KEY])
         return view(self, request, **kwargs)
     return wrapper
