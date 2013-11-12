15ba1c34ae1b49b59f68af6ab27f7620062ce57e
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-12 18:55:14 +0200

    common.cmd, common.mkdir

--- opendevelop/common/__init__.py
+++ opendevelop/common/__init__.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from models import DockerServer
 from models import OpenDevelopUser
+from os import makedirs
+from subprocess import call
+from subprocess import PIPE
 
 admin.site.register(DockerServer)
 admin.site.register(OpenDevelopUser, UserAdmin)
+
+def cmd(args):
+    return call(args, stdout=PIPE, stderr=PIPE)
+
+def mkdir(path):
+    makedirs(path)
+    cmd(['chmod', 'g+rwx', path])
--- opendevelop/sandboxes/models.py
+++ opendevelop/sandboxes/models.py
@@ -16,3 +16,9 @@ class Sandbox(models.Model):
 	image = models.ForeignKey('images.Image')
 	cmd = models.TextField()
 	status = models.CharField(choices=STATUSES, max_length=16, default=CREATED)
+
+
+class SandboxLog(models.Model):
+    sandbox = models.ForeignKey('sandboxes.Sandbox')
+    return_code = models.IntegerField()
+    logs = models.TextField()
