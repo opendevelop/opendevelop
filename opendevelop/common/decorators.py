b8a2fd2824e6a2efbe3da802c53f458a3dae018e
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-12 13:39:42 +0200

    Fixed Authorization header cheacks

--- opendevelop/common/decorators.py
+++ opendevelop/common/decorators.py
@@ -1,4 +1,5 @@
 from common.views import HttpResponseUnauthorized
+from django.http import HttpResponseBadRequest
 from functools import wraps
 import base64
 
@@ -9,6 +10,13 @@ def oauth(view):
         if (not AUTH_HEADER_KEY in request.META):
             msg = 'This resource requires OAuth2 authorization.'
             return HttpResponseUnauthorized(msg)
-        auth_data = base64.standard_b64decode(request.META[AUTH_HEADER_KEY])
+        auth_header = request.META[AUTH_HEADER_KEY]
+        try:
+            auth_data = base64.standard_b64decode(auth_header)
+            auth_data = auth_data.split(':')
+            if (len(auth_data) <> 2):
+                raise Exception()
+        except Exception:
+            return HttpResponseBadRequest('Invalid Authorization header data.')
         return view(self, request, **kwargs)
     return wrapper
