522460697c7cd320dc18c63706aeec31e4fb8158
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-12 15:13:18 +0200

    Added client_id, client_secret length check

--- opendevelop/common/decorators.py
+++ opendevelop/common/decorators.py
@@ -13,6 +13,7 @@ def oauth(view):
             msg = 'This resource requires OAuth2 authorization.'
             return HttpResponseUnauthorized(msg)
         auth_header = request.META[AUTH_HEADER_KEY]
+
         try:
             (bearer_type, auth_data) = tuple(auth_header.split(' '))
             if (not bearer_type in ALLOWED_BEARER_TYPES):
@@ -20,10 +21,15 @@ def oauth(view):
                 msg = 'Incorrect bearer type. Available types: %s' % bearers
                 return HttpResponseBadRequest(msg)
             auth_data = base64.standard_b64decode(auth_data)
-            auth_data = auth_data.split(':')
-            if (len(auth_data) <> 2):
-                raise Exception()
+            (client_id, client_secret) = tuple(auth_data.split(':'))
         except Exception:
             return HttpResponseBadRequest('Invalid Authorization header data.')
+
+        if (len(client_id) <> 20):
+            return HttpResponseBadRequest('client_id should be 20 chars long.')
+
+        if (len(client_secret) <> 40):
+            return HttpResponseBadRequest('client_secret should be 40 chars long.')
+
         return view(self, request, **kwargs)
     return wrapper
