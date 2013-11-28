7524f73c6db323a16e7de5cda26a3b144eb03ba0
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-28 15:22:39 +0200

    Removed status from sandboxes.logic.create
    Removed status from invocation of sandboxes.logic.create since it was
    initialized to SandBox.CREATED, which is the default value for that
    field.

--- opendevelop/sandboxes/logic.py
+++ opendevelop/sandboxes/logic.py
@@ -3,7 +3,7 @@ import tasks
 
 
 def create(app, cmd, image, files):
-    sandbox = Sandbox.objects.create(status=Sandbox.CREATED, owner_app=app,
+    sandbox = Sandbox.objects.create(owner_app=app,
                                      cmd=str(cmd), image=image)
     r = tasks.run_code.delay(sandbox.id, cmd, files)
     return sandbox.id
