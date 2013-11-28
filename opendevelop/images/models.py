611ae09bd5c279838a3fa63b58b5fc799e224494
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-11-28 19:03:24 +0200

    Added images migrations

--- opendevelop/images/models.py
+++ opendevelop/images/models.py
@@ -9,5 +9,8 @@ class Image(models.Model):
     name = models.CharField(max_length=32)
     slug = models.SlugField(max_length=32)
     description = models.TextField()
-    url = models.URLField(max_length=64, default=None)
+    url = models.URLField(max_length=64, blank=True, null=True, default='')
     docker_image_name = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.name
