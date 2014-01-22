4c56db46db82238cb4107b87749a239805b97b25
Paris Kasidiaris <pariskasidiaris@gmail.com>
2014-01-22 02:15:07 +0000

    Fix #40

--- opendevelop/opendevelop/settings.py
+++ opendevelop/opendevelop/settings.py
@@ -133,12 +133,17 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_nose',
     'south',
     'common',
     'api',
     'images',
     'sandboxes',
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = ['--with-xunit']
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
