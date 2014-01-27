58187ed42f631e28b5ab014d8c51635d507d30a8
Paris Kasidiaris <pariskasidiaris@gmail.com>
2014-01-27 19:29:05 +0000

    Fix typo & code styling

--- opendevelop/common/management/commands/server-create.py
+++ opendevelop/common/management/commands/server-create.py
@@ -4,23 +4,23 @@ from common.models import DockerServer
 
 class Command(BaseCommand):
 
-    help = "Add a new OpenDevelop Server"
+    help = 'Add a new OpenDevelop Server'
 
     option_list = BaseCommand.option_list + (
         make_option(
             '--name',
             dest='name',
-            help="Specify a name for the new server"),
+            help='Specify a name for the new server'),
         make_option(
             '--url',
             dest='url',
-            help="Specify a url to connect for management. Default is "
-                  "unixL//run/docker.sock"),
+            help='Specify a url to connect for management. Default is '
+            'unix://run/docker.sock'),
         make_option(
             '--buckets',
             dest='buckets',
-            help="Specify directory to place the created buckets. Default is "
-                  "/etc/opendevelop/buckets"),
+            help='Specify directory to place the created buckets. Default is '
+                  '/etc/opendevelop/buckets'),
     )
 
     def handle(self, *args, **options):
@@ -30,7 +30,7 @@ class Command(BaseCommand):
 
         args = dict()
         if name is None:
-            raise CommandError("Please specify a name for the new server")
+            raise CommandError('Please specify a name for the new server')
         args['name'] = name
 
         if url:
@@ -40,4 +40,4 @@ class Command(BaseCommand):
             args['bucket_list']=buckets_dir
 
         server = DockerServer.objects.create(**args)
-        self.stdout.write("Succesully created server %s\n" % server)
+        self.stdout.write('Succesully created server %s\n' % server)
