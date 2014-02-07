466cc45aa51cdc64d15a77ea818538ab374fc0d9
Antonis Kalipetis <akalipetis@gmail.com>
2014-02-07 20:20:12 +0200

    Updates API urls.py to be in plural
    Fixes #68

--- opendevelop/api/urls.py
+++ opendevelop/api/urls.py
@@ -5,6 +5,6 @@ from sandboxes.views import SandboxListView, SandboxSingleView
 urlpatterns = patterns(
     '',
     url(r'^images/?$', ImagesView.as_view()),
-    url(r'^sandbox/?$', SandboxListView.as_view()),
-    url(r'^sandbox/(\w+)?$', SandboxSingleView.as_view()),
+    url(r'^sandboxes/?$', SandboxListView.as_view()),
+    url(r'^sandboxes/(\w+)?$', SandboxSingleView.as_view()),
 )
